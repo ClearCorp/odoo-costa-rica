9d23249b53e95c2520dddb626b326b9f1b56956e
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2014-12-18 16:07:00 -0600

    [MIGRATE]l10n_cr_hr_payroll

---
+++ l10n_cr_hr_payroll /__openerp__.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Hr Payroll Localization - Costa Rica',
+    'version': '1.0',
+    'category': 'Localization',
+    "sequence": 38,
+    'complexity': "normal",
+    'description': """
+Hr Payroll Localization - Costa Rica.
+======================================
+Description
+------------
+This is the Costa Rica OpenERP localization and consists of:
+    - Salary Rule Structure
+    - Salary Rule Category
+    - Withholding tax structure
+    - Rent (Applied to Costa Rican's Law)
+TODO:
+    - Reports
+    """,
+    'author': 'ClearCorp',
+    'website': 'http://www.clearcorp.co.cr',
+    'depends': [
+        'hr_payroll_extended',
+    ],
+    'data': [
+             ''
+             'data/l10n_cr_hr_payroll_salary_rule_category.xml',
+             'data/l10n_cr_hr_payroll_salary_rule.xml',
+             'data/report_paperformat.xml',
+             'wizard/hr_payroll_report_for_month_wizard_view.xml',
+             'wizard/hr_payroll_report_employee_by_periods_wizard_view.xml',
+             'hr_config_settings.xml',
+             'l10n_cr_hr_payroll_view.xml',
+             'views/report_employee_by_periods.xml',
+             'views/report_employee_by_mounths.xml',
+             'views/report_payslipdetails.xml',
+             'views/report_payslip_run.xml',
+             'views/report_payslip.xml',
+             'report/report.xml',
+             'report/report_menus.xml',
+             'security/ir.model.access.csv',
+            ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ l10n_cr_hr_payroll /wizard/hr_payroll_report_for_month_wizard.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Addons modules by CLEARCORP S.A.
+#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from openerp import models, fields, api, _
+
+class PayrollReportForMonthWizard(models.TransientModel):
+
+    _name = "payroll.report.for.month"
+    _description = "Payroll Report for Month"
+    
+    company_id= fields.Many2one('res.company', 'Company',)
+    period_from= fields.Many2one('account.period', 'Start Period',)
+    period_to= fields.Many2one('account.period', 'End Period',)
+      
+    _defaults = {
+        'company_id': lambda self, cr, uid, context: \
+                self.pool.get('res.users').browse(cr, uid, uid,
+                    context=context).company_id.id,
+    }
+    
+    @api.multi
+    def print_report(self):
+        #if not self.company_id:
+         #   self.company_id = self.env['res.partner'].search([('customer','=',True)])
+        p_from= self.env['account.period'].search([('id','=',self.period_from.id)])[0].date_start
+        p_to= self.env['account.period'].search([('id','=',self.period_to.id)])[0].date_stop
+        #company_id = [empleado.id for partner in wizard.company_id]
+        data = {
+            'form': {
+                'period_from':p_from,
+                'period_to': p_to,
+            }
+        }
+        res = self.env['report'].get_action(self.company_id,
+            'l10n_cr_hr_payroll.report_employee_by_mounths', data=data)
+        return res
+    ##########################################################################
+"""        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'hr_payroll_report_for_month',
+            'datas': datas}
+
+    def action_validate(self, cr, uid, ids, context={}):
+        datas = {}
+        datas['ids'] = context.get('active_ids', [])
+        datas['model'] = context.get('active_model', 'ir.ui.menu')
+        datas['form'] = self.read(cr, uid, ids, ['company_id',  'period_from', 'period_to'], context=context)[0]
+        return self._print_report(cr, uid, ids, datas, context=context)"""
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
